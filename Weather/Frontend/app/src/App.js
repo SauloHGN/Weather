0d3e2b68326088c704b3bde6e3215795a2e3083f
Saulo <saulohgn@gmail.com>
2023-08-18 17:46:17 -0300

    ...

--- Weather/Frontend/app/src/App.js
+++ Weather/Frontend/app/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() 
+{
   return (
     <div className="App">
       <header className="App-header">
-       
+       Teste
       </header>
     </div>
   );
